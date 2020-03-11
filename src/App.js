bf6d79d99fb8e6e03bd3cce84ab22fc4d6a6816c
Lee kiseop <dlrltjq1004@gmail.com>
2020-03-12 04:12:20 +0900

    Portals Error Bouncaries

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BoundaryHOC = ProtectedComponent =>
+  class Bounder extends Component {
+    state = {
+      hasError: false
+    };
+    componentDidCatch = (error, info) => {
+      this.setState({
+        hasError: true
+      });
+    };
+    render() {
+      const { hasError } = this.state;
+      if (hasError) {
+        return <ErrorFallback />;
+      } else {
+        return <ProtectedComponent />;
+      }
+    }
+  };
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    });
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
 }
 
-export default App;
+const PErrorMaker = BoundaryHOC(ErrorMaker);
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const PPortals = BoundaryHOC(Portals);
+
+const Message = () => "Just touched it!";
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
+
+const ErrorFallback = () => "Sorry something went wrong";
+class App extends Component {
+  render() {
+    const { hasError } = this.state;
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <PPortals />
+        <PErrorMaker />
+      </Fragment>
+    );
+  }
+}
+
+export default BoundaryHOC(App);
